f11f7db5e1b4cbf5252d10d4e3040fd981b94137
QuatroQuatros <guicossalter@gmail.com>
2022-05-30 00:32:37 -0300

    fix rodapé

--- index.php
+++ index.php
@@ -556,8 +556,8 @@
 
           <div class="col-lg-4 col-md-6 footer-info">
             <h3>Uniwise</h3>
-            <p>A Uniwise é uma ong de doação de agasalhos na qual que o seu nome combina as palavras "união" e "wise",
-              que significa "sábio".</p>
+            <p>A Uniwise é um projeto de arrecadação de agasalhos para doação, criado pelos alunos do 3°DS, na qual seu nome combina as palavras "união" e "wise",
+              que significa "sábio" em inglês.</p>
           </div>
 
           <div class="col-lg-2 col-md-6 footer-links">
